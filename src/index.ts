b6dbaca0e0d87f5cede99ed6968bb07496a74204
yamasaw <tonigthisyamada@yamasaw.com>
2024-08-21 19:10:11 +0900

    CRUDのパターンを網羅

--- src/index.ts
+++ src/index.ts
@@ -37,6 +37,45 @@ app.get('/user/', async (c) => {
   return c.json(users)
 })
 
+app.post('/user/', async (c) => {
+  const data = await c.req.json()
+  const user = await client().user.create({
+    data: data
+  })
+  return c.json(user)
+})
+
+app.get('/user/:id/', async (c) => {
+  const id = c.req.param('id')
+  const user = await client().user.findUnique({
+    where: {
+      id: Number(id)
+    }
+  })
+  return c.json(user)
+})
+
+app.put('/user/:id/', async (c) => {
+  const data = await c.req.json()
+  const user = await client().user.update({
+    where: {
+      id: Number(c.req.param('id'))
+    },
+    data: data
+  })
+  return c.json(user)
+})
+
+app.delete('/user/:id/', async (c) => {
+  const id = c.req.param('id')
+  const user = await client().user.delete({
+    where: {
+      id: Number(id)
+    }
+  })
+  return c.json(user)
+})
+
 app.notFound((c) => {
   return c.json({ message: 'Not Found' }, 404)
 })
